1243e09fdbe188a6868b8bf8304ef9d289e06bdb
Ahmad Fahim <ahmadfahim2k@gmail.com>
2025-08-14 15:32:54 +0530

    fixed file paths in load method

--- Healthcare Premium (Regression)/app/prediction_helper.py
+++ Healthcare Premium (Regression)/app/prediction_helper.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from joblib import load
 
-model_rest = load("artifacts\model_rest.joblib")
-model_young = load("artifacts\model_young.joblib")
+model_rest = load("artifacts/model_rest.joblib")
+model_young = load("artifacts/model_young.joblib")
 
-scaler_rest = load("artifacts\scaler_rest.joblib")
-scaler_young = load("artifacts\scaler_young.joblib")
+scaler_rest = load("artifacts/scaler_rest.joblib")
+scaler_young = load("artifacts/scaler_young.joblib")
 
 expected_columns = [ 'age', 'number_of_dependants', 'income_lakhs', 'insurance_plan', 'genetical_risk', 'normalized_risk_score', 'gender_Male', 'region_Northwest', 'region_Southeast', 'region_Southwest', 'marital_status_Unmarried', 'bmi_category_Obesity', 'bmi_category_Overweight', 'bmi_category_Underweight', 'smoking_status_Occasional', 'smoking_status_Regular', 'employment_status_Salaried', 'employment_status_Self-Employed']
 
